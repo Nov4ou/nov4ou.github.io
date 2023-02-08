41c1452b05ef600106233d6e839e7a191eee06b4
Nov4ou <nov4ou@outlook.com>
2023-02-09 01:23:57 +0800

    Initial commit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230202.153818';
+const cacheName = 'chirpy-20230209.012013';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
