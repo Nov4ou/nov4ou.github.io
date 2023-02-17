ca551befb07e54c1f73f9093aa9a7fef21950813
Nov4ou <nov4ou@outlook.com>
2023-02-17 15:12:31 +0800

    Initial commit

--- nov4ou.github.io/deploy.bat
+++ nov4ou.github.io/deploy.bat
@@ -1,4 +1,4 @@
-Xcopy "C:\Users\megas\Documents\Jekyll\jekyll-theme-chirpy\_site\*" "C:\Users\megas\Documents\Jekyll\jekyll-theme-chirpy\nov4ou.github.io\" /E
+Xcopy "C:\Users\megas\OneDrive\Documents\Jekyll\jekyll-theme-chirpy\_site\*" "C:\Users\megas\OneDrive\Documents\Jekyll\jekyll-theme-chirpy\nov4ou.github.io\" /E
 
 cd nov4ou.github.io
 
